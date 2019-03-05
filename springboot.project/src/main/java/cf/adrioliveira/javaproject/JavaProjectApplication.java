d74e955ed604793a55a6b7ec345c4cccc795750c
Adriano Oliveira <drprado2@gmail.com>
2019-03-05 17:50:47 -0300

    Criado projeto springboot com Hello World e com Swagger configurado.

---
+++ springboot.project/src/main/java/cf/adrioliveira/javaproject/JavaProjectApplication.java
@@ -0,0 +1,13 @@
+package cf.adrioliveira.javaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaProjectApplication.class, args);
+	}
+
+}
